0ec432cb3a0b81796d69143e5deada089a788fe4
Zachariah B. Farahany <zfarahany193@gmail.com>
2024-08-20 16:15:37 -0500

    Started making a test_uav.py file which is meant to test the proper functioning of the UAV.py file

---
+++ tests/uav_test.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+# Make the current working directory the directory above so that we can call our source python files
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.UAV import UAV
+
+def main():
+
+
+    # All starting inforation
+    drone_ip = None
+
+    if drone_ip is None:
+        uav = UAV()
+    else:
+        uav = UAV(drone_ip)
+
+    uav.connect()
+    
+    battery = uav.get_battery()
+    print(battery)
+    
+    uav.takeoff()
+
+
+
+if __name__ == "__main__":
+    print("Executing main")
+    main()
+    print("Executed main")
